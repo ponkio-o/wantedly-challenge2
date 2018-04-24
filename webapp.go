cdc7b7c601d128a4d15b4d9acb1eb26583f02dd4
ponkio-o <pasokon218@gmail.com>
2018-04-24 16:59:44 +0900

    機能分割

--- webapp.go
+++ webapp.go
@@ -15,6 +15,14 @@ func main() {
     r := mux.NewRouter()
 
     r.HandleFunc("/", getIndex).Methods("GET")
+    r.HandleFunc("/users", getList).Methods("GET")
+    r.HandleFunc("/users/{id}", getUser).Methods("GET")
+    r.HandleFunc("/users", postAdduser).Methods("POST")
+    r.HandleFunc("/users/{id}", putRefresh).Methods("PUT")
+    r.HandleFunc("/users/{id}", deleteUser).Methods("DELETE")
+
+    r.HandleFunc("/", getIndex).Methods("GET")
+
     // 8080ポートで起動
     http.Handle("/", r)
     http.ListenAndServe(":8080", nil)
@@ -36,4 +44,25 @@ func getIndex(w http.ResponseWriter, r *http.Request){
 
     // jsonデータを出力
     fmt.Fprint(w, string(outputJson))
+}
+
+
+func getList(w http.ResponseWriter, r *http.Request){
+  //ユーザーリスト表示
+}
+
+func getUser(w http.ResponseWriter, r *http.Request){
+  //ユーザー情報(ID指定)表示
+}
+
+func postAdduser(w http.ResponseWriter, r *http.Request){
+  //リスト表示
+}
+
+func putRefresh(w http.ResponseWriter, r *http.Request){
+  //更新
+}
+
+func deleteUser(w http.ResponseWriter, r *http.Request){
+  //削除
 }
